b1e1583199b7cb1a2cfa076659e611043246b2bc
softech86 <leobai@baiyifeideMacBook-Pro.local>
2018-04-14 02:26:37 +0800

    [+]: add shortern version

--- DateToChinese.js
+++ DateToChinese.js
@@ -1,6 +1,6 @@
 Date.prototype.toChinese = function (template) {
     var d = this
-    template = template || '%y年%M月%d日 %H点%m分%s秒'
+    template = template || '%y年%M月%d日 %H点%m分%s秒' // 填充模板
     var date = [
         {
             key: /%y/g,
@@ -26,7 +26,7 @@ Date.prototype.toChinese = function (template) {
             key: /%s/g,
             value: numberToChinese(d.getSeconds(), false, true)
         }
-    ]
+    ] // 替换规则
     return date
         .reduce(function (s, x) {
             return s.replace(x.key, x.value)
@@ -34,7 +34,7 @@ Date.prototype.toChinese = function (template) {
         .replace(/(零分)?零秒/g, '整')
 }
 
-function numberToChinese(num, simple, zero) {
+function numberToChinese(num, simple, zero) { // 百位内数字转中文
     num = parseInt(num);
     simple = simple === true
     zero = zero === true
@@ -59,14 +59,26 @@ function numberToChinese(num, simple, zero) {
     }
 }
 
+Date.prototype.toChineseShort = function (template) { // 一个代码长度压缩的版本，详见文档
+    var d = new Date(this)
+    d.setHours(d.getHours() - d.getTimezoneOffset() / 60)
+    var t = d.toISOString().split(/-|T|:|\./g).splice(0, 6)
+    var ch = '零一二三四五六七八九'.split(''), after = '年,月,日 ,点,分,秒'.split(',')
+    return t.map((x, i) => [].map.call('' + parseInt(x), y => ch[y]).join(i ? '十' : '') + after[i]).join('').replace(/一?十零?/g, '十').replace(/(零分)?零秒/g, '整').replace(/(时|分)([^零])(分|秒)/g, '$1零$2$3')
+}
 
 // test
 
-console.log([0, 9, 10, 12, 40, 99].map(numberToChinese))
+console.log([0, 9, 10, 12, 40, 99].map(numberToChinese)) // ['零', '九', '十', '十二', '四十', '九十九' ]
 
-console.log(new Date('2018-3-2 01:0:01').toChinese())
-console.log(new Date('2018-3-10 15:0:0').toChinese())
-console.log(new Date('2016-12-25 0:32:07').toChinese())
-console.log(new Date('2016-1-1 0:0:0').toChinese())
+console.log(new Date('2018-3-2 01:0:01').toChinese()) // 二零一八年三月二日 一点零分零一秒
+console.log(new Date('2018-3-10 15:0:0').toChinese()) // 二零一八年三月十日 十五点整
+console.log(new Date('2016-12-25 0:32:07').toChinese()) // 二零一六年十二月二十五日 零点三十二分零七秒
+console.log(new Date('2016-1-1 0:0:0').toChinese()) // 二零一六年一月一日 零点整
 
-console.log(new Date().toChinese('今天是%y年%M月%d日呀~'))
+console.log(new Date().toChinese('今天是%y年%M月%d日呀~')) // 今天是二零一八年四月十四日呀~
+
+console.log(new Date('2018-3-2 01:0:01').toChineseShort()) // 二零一八年三月二日 一点零分零一秒
+console.log(new Date('2018-3-10 15:0:0').toChineseShort()) // 二零一八年三月十日 十五点整
+console.log(new Date('2016-12-25 0:32:07').toChineseShort()) // 二零一六年十二月二十五日 零点三十二分零七秒
+console.log(new Date('2016-1-1 0:0:0').toChineseShort()) // 二零一六年一月一日 零点整
--- README.md
+++ README.md
@@ -3,10 +3,47 @@ Convert UTC time to Beijing Time in Chinese
 
 ## Basic
 ```
-d2c = new Date('2018-3-10 15:0:0').toChinese()
+d2c = new Date('2018-3-10 15:0:0').toChinese() // 二零一八年三月十日 十五点整
 ```
 
 ## Using Template
 ```
-d2c = new Date().toChinese('今天是%y年%M月%d日呀~')
+d2c = new Date().toChinese('今天是%y年%M月%d日呀~') // 今天是二零一八年四月十三日呀~
+```
+
+## Shortern Version
+```
+Date.prototype.toChineseShort = function (template) {
+    var d = new Date(this)
+    d.setHours(d.getHours() - d.getTimezoneOffset() / 60)
+    var t = d.toISOString().split(/-|T|:|\./g).splice(0, 6) // 获取时间信息数组
+
+    var ch = '零一二三四五六七八九'.split(''),
+        after = '年,月,日 ,点,分,秒'.split(',')
+    return t
+        .map(function (x, i) {
+            var res = [].map.call('' + parseInt(x), function (y) {
+                return ch[y]
+            }) // 数字转中文
+            return 
+                (i ? 
+                    res.join('十').replace(/一?十零?/g, '十') : // 转为数字正常读法
+                    res.join('')) // 年份直接连接数字
+                 + after[i] // 加时间单位
+        })
+        .join('')
+        .replace(/(零分)?零秒/g, '整') // 几点零分零秒 => 几点整
+        .replace(/(时|分)([^零])(分|秒)/g, '$1零$2$3') // 几点几分 => 几点零几分
+}
+```
+
+使用 es6 箭头函数缩写，稍微调整，有
+```
+Date.prototype.toChineseShort = function (template) {
+    var d = new Date(this)
+    d.setHours(d.getHours() - d.getTimezoneOffset() / 60)
+    var t = d.toISOString().split(/-|T|:|\./g).splice(0, 6)
+    var ch = '零一二三四五六七八九'.split(''), after = '年,月,日 ,点,分,秒'.split(',')
+    return t.map((x, i) => [].map.call('' + parseInt(x), y => ch[y]).join(i ? '十' : '') + after[i]).join('').replace(/一?十零?/g, '十').replace(/(零分)?零秒/g, '整').replace(/(时|分)([^零])(分|秒)/g, '$1零$2$3')
+}
 ```
